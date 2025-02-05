2da9ce4eae786e0e93890a06f30b633b41756b48
Benjamin Harris <Bigben83@users.noreply.github.com>
2025-02-06 10:32:54 +1100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -24,6 +24,9 @@ end
 # Step 2: Parse the page content using Nokogiri
 doc = Nokogiri::HTML(page_html)
 
+# Print out a snippet of the HTML for debugging
+logger.info("HTML Content snippet: #{doc.to_html[0..500]}")
+
 # Step 3: Initialize the SQLite database
 db = SQLite3::Database.new "data.sqlite"
 
