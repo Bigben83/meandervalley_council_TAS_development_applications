ebe533a5366d15479ee9324387c735a6df04e18e
Benjamin Harris <Bigben83@users.noreply.github.com>
2025-02-06 11:25:27 +1100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,10 +1,23 @@
-require 'selenium-webdriver'
+# Require library
+require "capybara"
+require "selenium-webdriver"
 
-# Set up the WebDriver for Chrome (Morph.io supports it)
-driver = Selenium::WebDriver.for :chrome
+Capybara.register_driver :selenium_chrome_headless_morph do |app|
+  Capybara::Selenium::Driver.load_selenium
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+    opts.args << '--headless'
+    opts.args << '--disable-gpu' if Gem.win_platform?
+    # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
+    opts.args << '--disable-site-isolation-trials'
+    opts.args << '--no-sandbox'
+  end
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+end
 
-# Navigate to the target page
-driver.get "https://www.meander.tas.gov.au/advertised-approved-planning-applications/"
+# Open a Capybara session with the Selenium web driver for Chromium headless
+driver = Capybara::Session.new(:selenium_chrome_headless_morph)
+
+driver.visit("https://www.meander.tas.gov.au/advertised-approved-planning-applications/")
 
 # Wait for the page to load (you might need to adjust the waiting time)
 driver.manage.timeouts.implicit_wait = 10 # waits for 10 seconds
